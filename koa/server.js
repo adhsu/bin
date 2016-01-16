2f802acb1506af9814e26c48867e51c5fb552818
Connor Zwick <connor@Nicks-iMac.local>
2016-01-15 16:39:07 -0800

    koa server can now get titles and metadata for URLs but not direct image links

--- koa/server.js
+++ koa/server.js
@@ -1,15 +1,53 @@
 var koa = require('koa')
 var router = require('koa-router')
 var mount = require('koa-mount')
+var thunkify = require('thunkify')
 var app = koa()
 
-var handler = function *(next){
-  this.body = {'api-status': 'working'}
+
+
+
+var MetaInspector = require('node-metainspector');
+
+// Thank the Lord for http://zef.me/blog/6096/callback-free-harmonious-node-js
+
+var getURL = function *(next){
+
+  var url = this.url.replace('/title/?url=','')
+
+  var client = new MetaInspector(url, { timeout: 5000 });
+
+  // var fetch = thunkify(client.on('fetch'))
+
+  var clientListener = function(){
+    return function(callback) {
+      client.on('fetch', callback)
+    }
+  }
+
+  client.fetch()
+
+  yield clientListener()
+  // try {
+  //   throw "file too large"
+  // }
+  // catch (e) {
+  //   console.log(e)
+  //   this.body = 'File too large'
+  // }
+
+
+  this.body = {'title': client.title, 'ogTitle': client.ogTitle, 'host': client.host, 'image': client.image}
+  // console.log(client)
+
+
+
+  
 }
 
-var API = new router()
-API.get('/test', handler)
 
+var API = new router()
+API.get('/title/', getURL)
 
 // // x-response-time
 
